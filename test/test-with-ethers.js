1f6323cc75c4d763a42ba562d09b7239b91ae635
Dror Tirosh <dror@opengsn.org>
2020-05-16 16:29:40 +0300

    send from gasless account
    show how to send from a new, completely gasless account

--- test/test-with-ethers.js
+++ test/test-with-ethers.js
@@ -18,42 +18,43 @@ describe('using ethers with OpenGSN', () => {
 	let env = await GsnTestEnvironment.startGsn('localhost')
 	const { relayHubAddress, paymasterAddress, stakeManagerAddress, forwarderAddress } = env.deploymentResult
 
-        web3provider = new Web3HttpProvider('http://localhost:8545')
-        const etherProvider1 = new ethers.providers.Web3Provider(web3provider)
+    
+        const web3provider = new Web3HttpProvider('http://localhost:8545')
+ 
+        const deploymentProvider= new ethers.providers.Web3Provider(web3provider)
 
-        accounts = await etherProvider1.listAccounts()
-
-        from = accounts[0]
-        const factory = new ethers.ContractFactory(Counter.abi, Counter.bytecode, etherProvider1.getSigner())
+        const factory = new ethers.ContractFactory(Counter.abi, Counter.bytecode, deploymentProvider.getSigner())
 
         counter = await factory.deploy(forwarderAddress)
         await counter.deployed()
 
         const config = configureGSN({
-            verbose: false,
+            // verbose: true,
             relayHubAddress,
             stakeManagerAddress,
             paymasterAddress,
         })
-        const gsnProvider = new RelayProvider(web3provider, config)
-	// The above is the full provider configuration. can use the provider returned by startGsn:
+        // const hdweb3provider = new HDWallet('0x123456', 'http://localhost:8545')
+        let gsnProvider = new RelayProvider(web3provider, config)
+	   // The above is the full provider configuration. can use the provider returned by startGsn:
         // const gsnProvider = env.relayProvider
+
+    	const account = gsnProvider.newAccount()
+    	from = account.address
+
+        // gsnProvider is now an rpc provider with GSN support. make it an ethers provider:
         const etherProvider = new ethers.providers.Web3Provider(gsnProvider)
 
-        counter = counter.connect(etherProvider.getSigner())
-
+        counter = counter.connect(etherProvider.getSigner(from))
     })
 
     describe('make a call', async () => {
         let counterChange
         let balanceUsed
         before(async () => {
-            const beforeBalance = await counter.provider.getBalance(from)
             const countBefore = await counter.counter()
             await counter.increment()
             const countAfter = await counter.counter()
-            const afterBalance = await counter.provider.getBalance(from)
-            balanceUsed = beforeBalance - afterBalance
             counterChange = countAfter - countBefore
         })
 
@@ -62,12 +63,12 @@ describe('using ethers with OpenGSN', () => {
             assert.equal(1, counterChange)
         })
 
-        it('should not pay for gas', async () => {
-            assert.equal(0, balanceUsed)
+        it('should not pay for gas (balance=0)', async () => {
+            assert.equal(0, await counter.provider.getBalance(from))
         })
 
         it('should see the real caller', async () => {
-            assert.equal(from, await counter.lastCaller())
+            assert.equal(from.toLowerCase(), (await counter.lastCaller()).toLowerCase())
         });
     })
 })
